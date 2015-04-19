7383ddde4f112007395dad524c5f61512b036f71
David Abramowitz <david.xesllc@gmail.com>
2015-04-19 15:44:48 -0600

    Chapter 1

---
+++ chapter1.js
@@ -0,0 +1,39 @@
+// looping triangle
+
+for (var i = 0; i <= 8 ; ++i) {
+  var string = "########";
+  console.log(string.slice(0,i));
+}
+
+// fizz buzz
+
+for (var i = 1; i <= 100; i++) {
+  if (i/3%5 === 0 ) {
+    console.log("FizzBuzz");
+  }
+  else if (i%5 === 0) {
+    console.log ("Buzz");
+  }
+  else if (i%3 === 0) {
+   console.log("Fizz");
+ }
+  else {
+    console.log(i);
+  };
+};
+
+// chess board
+
+var size = 8
+var chess_board = ""
+for (var i = 0; i < size/2; i++) {
+  for (var j = 0; j < (size/2 ); j++) {
+    chess_board += "# ";
+  };
+  chess_board += "\n";
+  for (var k = 0; k < size/2; k++) {
+    chess_board += " #";
+  };
+  chess_board += "\n"
+};
+console.log(chess_board.substring(0, chess_board.length - 1))
